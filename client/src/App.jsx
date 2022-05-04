69c5e0a8c00f7c25d97a330b9a6d4e92f5dca8fd
theboringschool <theboringschool.org@gmail.com>
2022-05-04 12:48:45 +0545

    Working on Swap Form

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import './styles/app.css'
+
+export default function App() {
+  return (
+    <div className='app__container'>
+      <Navbar/>
+    </div>
+  )
+}
