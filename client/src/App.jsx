d361604e60546fbb89f8ad6c2a93e4944b78295e
theboringschool <theboringschool.org@gmail.com>
2022-05-04 14:32:17 +0545

    Working on Button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/navbar'
+import Swap from './components/Swap'
 import './styles/app.css'
 
 export default function App() {
   return (
     <div className='app__container'>
       <Navbar/>
+      <Swap/>
     </div>
   )
 }
