c85649a22e101ae52423223730e8c310e9814ff9
theboringschool <theboringschool.org@gmail.com>
2022-05-10 05:00:29 -0700

    Recovered Existing Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import "./styles/App.css";
+import Navbar from './Components/Navbar'
+import Swap from './Components/Swap'
 export default function App() {
   return (
-    <div>App</div>
+    <div className="app__container">
+      <Navbar />
+      <Swap/>
+    </div>
   )
 }
---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./styles/style.css";
+import Button from "@mui/material/Button";
+
+export default function Navbar() {
+  return (
+    <div className="navbar__container">
+      <img src="https://ik.imagekit.io/lexworld/Logo.png" alt="Playpoint.ai" />
+
+      <div className="utilities">
+        <Button className="network">
+          <img
+            src="https://uxwing.com/wp-content/themes/uxwing/download/10-brands-and-social-media/avalanche-avax.png"
+            alt=""
+          />
+          <span>Avalanche</span>
+          <span>
+            <i className="ri-arrow-down-s-line"></i>
+          </span>
+        </Button>
+
+        {/* {
+          userPublicAddress !== "" ? (
+            <Button className="network" onClick={connectWallet}>
+              <img
+
+                src="https://ik.imagekit.io/lexworld/Logo.png"
+                alt=""
+              />
+              <span>Disconnect</span>
+              <span>
+                <i className="ri-arrow-down-s-line"></i>
+              </span>
+            </Button>
+          ) : (
+            <Button className="network" onClick={connectWallet}>
+              <img
+
+                src="https://ik.imagekit.io/lexworld/Logo.png"
+                alt=""
+              />
+              <span>Connect</span>
+              <span>
+                <i className="ri-arrow-down-s-line"></i>
+              </span>
+            </Button>
+          )
+        } */}
+        <Button className="downIcon">
+          <i className="ri-menu-3-line"></i>
+        </Button>
+      </div>
+    </div>
+  );
+}
